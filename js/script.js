28ff0f0039bd2b16937f9551d6caf8bef9877584
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-08 13:58:01 +0100

    create variable i and start cicle while

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ const shoppingList = ['Riso', 'Pasta', 'Pane', 'Cereali', 'Latte', 'Pollo']
 const listElement = document.getElementById('lista-spesa')
 //3-Creo la variabile che conterrà l'ul
 let list = `<ul>`
+let i = 0
+//5-Utilizzo il ciclo while per creare tutti gli elementi della lista e inserirli nell ul 
+while (i < shoppingList.length) {
+    list += `<li>${shoppingList[i]}</li>`
+    i++;
+}
