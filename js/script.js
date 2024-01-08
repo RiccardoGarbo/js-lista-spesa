820fdda9a28b1ed99a038cf2b2241aeb47849182
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-08 13:56:34 +0100

    create variable-link file html

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 //1-Creo la variabile array con tutti gli elementi della lista
 const shoppingList = ['Riso', 'Pasta', 'Pane', 'Cereali', 'Latte', 'Pollo']
+//2-Creo la variabile che conterrà la lista nel documento HTML
+const listElement = document.getElementById('lista-spesa')
